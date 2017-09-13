0c415a4c58dbd38d6ed4b766bc7eff8662b74574
Nur Izdihar Binte Ismail <nurizdiharismail@gmail.com>
2017-09-13 03:17:10 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -94,7 +94,7 @@ end
 def draw?(board)
   if !won?(board) && full?(board)
     return true
-  elsif !won?(board) && !full?(board)
+  elsif !won?(board) 
     return false
   elsif won?(board)
     return false
